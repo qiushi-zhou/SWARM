edd99a461cc67b7a97ece4c4cdc9d32400dfce4e
Gabriele Marini <giaga7@gmail.com>
2022-09-29 23:15:19 +1000

    Fixed mismatching rows/cols sizing in the collage

--- SWARM_Stelarc/Tests/OpenCVCameraTest.py
+++ SWARM_Stelarc/Tests/OpenCVCameraTest.py
@@ -31,17 +31,32 @@ class FPSCounter():
       if elapsed >= self.reset_time:
         self.reset()
 
+class OpenCVCameraPlaceholder:
+    def __init__(self, size, color=(0,0,0), border_size=5):
+        self.color = color
+        self.ready = True
+        self.initialized = True
+        self.size = size
+        self.b_size = int(border_size)
+        self.frame = np.full((size['height'], size['width'], 3), self.color, np.uint8)
+        self.frame = cv2.copyMakeBorder(self.frame, top=self.b_size, bottom=self.b_size, left=self.b_size, right=self.b_size, borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        self.is_placeholder = True
+
+    def get_frame(self):
+        return self.frame
+
 
 class OpenCVCamera:
-    def __init__(self, cam_id, size, cam_name=None):
+    def __init__(self, cam_id, size, cap_param, cam_name=None, border_size=5):
         self.size = size
+        self.b_size = int(border_size)
         self.cam_id = cam_id
         self.cam_name = cam_name
+        self.cap_param = cap_param
         if self.cam_name is None:
             self.cam_name = f'Camera {cam_id}'
         self.cam = None
         self.last_frame = None
-        self.cam = None
         self.fps_counter = FPSCounter()
         self.thread = threading.Thread(target=self.capture_loop, args=[])
         self.thread_started = True
@@ -52,18 +67,14 @@ class OpenCVCamera:
         self.last_thread_name = "None"
         self.frame_shape = [-1, -1]
         self.ready = False
+        self.is_placeholder = False
 
     def capture_loop(self):
         res_str = f"Cap {self.cam_id}\t{self.cam_name}\t{self.thread.name}"
         try:
-            if platform == "win32":
-                self.cam = cv2.VideoCapture(self.cam_id, cv2.CAP_DSHOW)
-            else:
-                # On MacOS, make sure to install opencv with "brew install opencv" and then link it with "brew link --overwrite opencv"
-                # Also remove CAP_DSHOW for MacOS
-                self.cam = cv2.VideoCapture(self.cam_id, cv2.CAP_AVFOUNDATION)
+            self.cam = cv2.VideoCapture(self.cam_id, self.cap_param)
         except Exception as e:
-            print(f"Error opening VideoCapture {self.cam_id}")
+            print(f"Error opening VideoCapture {self.cam_id}: {e}")
             pass
         self.initialized = True
         if self.cam is None or not self.cam.isOpened():
@@ -88,11 +99,10 @@ class OpenCVCamera:
                 time.sleep(0.001)
         self.cam.release()
 
-    def process_frame(self, frame, dbg_info=True):
-        b_size = 5
+    def process_frame(self, frame):
         if frame is not None:
             frame = cv2.resize(frame, (self.size['width'], self.size['height']))
-            frame = cv2.copyMakeBorder(frame, top=b_size, bottom=b_size, left=b_size, right=b_size, borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255])
+            frame = cv2.copyMakeBorder(frame, top=self.b_size, bottom=self.b_size, left=self.b_size, right=self.b_size, borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255])
         return frame
 
     def get_frame(self, dbg_info=True):
@@ -109,7 +119,6 @@ class OpenCVCamera:
                 font_scaling = 0.35
                 for idx in range(0, len(lines)):
                     frame = cv2.putText(frame, lines[idx], (10, int(y_offset * (idx+1) * font_scaling)), cv2.FONT_HERSHEY_SIMPLEX, font_scaling, (0, 255, 0), 1, cv2.LINE_AA)
-
         return frame
 
 class OpenCVCamerasManager:
@@ -121,69 +130,100 @@ class OpenCVCamerasManager:
         self.cameras = []
         self.max_idx = max_idx
         self.print_delay = 5
-        self.last_print_time = time.time() - ((self.print_delay-1))
+        self.last_print_time = time.time() - (self.print_delay-1)
+        self.cap_param = cv2.CAP_DSHOW
         cv2.namedWindow(self.title)
 
     def init_cameras(self):
         devices = []
         if platform == 'win32':
+            self.cap_param = cv2.CAP_DSHOW
+            print(f"\nRunning on Windows {platform}: using cv2.CAP_DSHOW\n\n")
             from pygrabber.dshow_graph import FilterGraph
             graph = FilterGraph()
             devices = graph.get_input_devices()
+            self.print_delay = 1
+        else:
+            print(f"\nRunning on MacOS {platform}: using cv2.CAP_AVFOUNDATION\n\n")
+            self.cap_param = cv2.CAP_AVFOUNDATION
             self.print_delay = 1
         max_idx = self.max_idx if len(devices) <= 0 else len(devices)
         for idx in range(0, max_idx):
             cam_name = f'Camera {idx}'
             if idx < len(devices):
                 cam_name = devices[idx]
-            cam = OpenCVCamera(idx, self.size, cam_name)
+            cam = OpenCVCamera(idx, self.size, self.cap_param, cam_name)
             self.cameras.append(cam)
+
+        ### Test
+        # self.cameras.append(OpenCVCameraPlaceholder(self.size, (255, 0 ,0)))
+        # self.cameras.append(OpenCVCameraPlaceholder(self.size, (0, 255 ,0)))
+        # self.cameras.append(OpenCVCameraPlaceholder(self.size, (0, 0 ,255)))
+        # self.cameras.append(OpenCVCameraPlaceholder(self.size, (255, 0 ,0)))
+        # self.cameras.append(OpenCVCameraPlaceholder(self.size, (0, 255 ,0)))
+
+        self.placeholder = OpenCVCameraPlaceholder(self.size, (0, 0 ,0))
 
     def get_collage(self):
         total_frames = 0
-        vstacks = []
-        hstacks = []
+        vstack = []
+        hstack = []
         alive_threads = []
-        inactive_cams_idx = []
+        active_cams = []
         for i in range(0, len(self.cameras)):
             cam = self.cameras[i]
-            if cam.thread.is_alive():
-                alive_threads.append(f"{cam.cam_name} {cam.thread.name} {cam.total_frames}")
-            if cam.ready:
+            frame = None
+            if cam.is_placeholder:
+                active_cams.append(cam)
                 frame = cam.get_frame()
-                if frame is not None:
-                    hstacks.append(frame)
-                    total_frames += 1
-                if len(hstacks) >= self.cols:
-                    try:
-                        vstacks.append(np.hstack(hstacks))
-                    except Exception as e:
-                        print(f"Exception stacking horizontally: {e}")
             else:
-                if cam.initialized:
-                    inactive_cams_idx.append(i)
-        if len(inactive_cams_idx) > 0:
-            for cam_idx in inactive_cams_idx:
-                del self.cameras[cam_idx]
+                if cam.ready:
+                    active_cams.append(cam)
+                    frame = cam.get_frame()
+                    if cam.thread.is_alive():
+                        alive_threads.append(f"{cam.cam_name} {cam.thread.name} {cam.total_frames}")
+                elif not cam.initialized:
+                    active_cams.append(cam)
+                else:
+                    # Capture initialized but NOT ready, delete
+                    pass
+            if frame is not None:
+                hstack.append(frame)
+                total_frames += 1
+                if len(hstack) >= self.cols:
+                    vstack.append(hstack)
+                    hstack = []
+        self.cameras = active_cams
+        ## Square up the grid
+        if len(vstack) > 0 and len(hstack) > 0:
+            while len(hstack) < self.cols:
+                hstack.append(self.placeholder.get_frame())
+        if len(hstack) > 0:
+            vstack.append(hstack)
+        output = None
+        if len(vstack) > 0:
+            try:
+                hstacks = [np.hstack(h) for h in vstack]
+                try:
+                    output = np.vstack(hstacks)
+                except Exception as e:
+                    print(f"Exception Building hstack {len(hstack)} x {len(vstack)}: {e}")
+            except Exception as e:
+                print(f"Exception Building vstack {len(hstack)} x {len(vstack)}: {e}")
+
         if time.time() - self.last_print_time >= self.print_delay:
             self.last_print_time = time.time()
-            print(f"{datetime.datetime.now()}\tGrid {total_frames} : {len(hstacks)} x {len(vstacks)} \t{len(alive_threads)} / {len(self.cameras)} Alive Threads\t{alive_threads}", end='\n', flush=True)
-        try:
-            vstacks.append(np.hstack(hstacks))
-            return np.vstack(vstacks)
-        except Exception as e:
-            # print(f"Exception Building collage: {e}")
-            pass
+            print(f"{datetime.datetime.now()}\t\tGrid {total_frames} :{[len(h) for h in vstack]} \tThreads: {len(alive_threads)} / {len(self.cameras)}\t{alive_threads}", end='\n', flush=True)
+        return output
 
     def start_loop(self):
         while True:
-            output = None
             try:
                 output = self.get_collage()
-                cv2.imshow(self.title, output)
+                if output is not None:
+                    cv2.imshow(self.title, output)
             except Exception as e:
-                # print(f"Exception getting collage: {e}")
-                pass
+                print(f"Exception displaying image: {e}")
             time.sleep(0.001)
             key = cv2.waitKey(1)
             if key == 27:  # exit on ESC
